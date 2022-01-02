1217bad352a2567e9df92f969a6a90f777beec6b
Anas Mohamed <14171441+amohamed11@users.noreply.github.com>
2022-01-02 03:02:15 -0700

    feat: printing through serial port & custom tests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,81 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
 
 mod vga_buffer;
+mod serial;
+
 
 #[no_mangle] // maintain function name in compilation
 pub extern "C" fn _start() -> ! {
     // entry point for chibiOS
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
 
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+
+    exit_qemu(QemuExitCode::Failure);
+
+    loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,  // 33: (0x10 << 1) | 1
+    Failure = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn passing_trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn failing_trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(0, 1);
+    serial_println!("[ok]");
 }
